39f2c04fd50b73579e38bfb5922a8827bc60be32
dmclavel <dmclavel@up.edu.ph>
2019-02-11 15:03:16 +0800

    Added SocketIO events (connect and disconnect)

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 const path = require('path');
+const http = require('http');
 const publicPath = path.join(__dirname, '../public');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 const port = process.env.PORT || 3000;
 
 //Middlewares
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New user connected!');
+    // socket.emit
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    })
+});
+
+server.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
