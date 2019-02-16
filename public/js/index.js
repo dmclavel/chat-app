670d4f9b5787fb5b63aa1eb02f832f317328c282
dmclavel <dmclavel@up.edu.ph>
2019-02-16 15:17:51 +0800

    Scroll to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,21 @@
 const socket = io();   //initiate request from the client to the server
+
+function scrollToBottom () {
+    //Selectors
+    const messages = jQuery('#messages');
+    const newMessage = messages.children('li:last-child');
+
+    //Heights
+    const clientHeight = messages.prop('clientHeight');
+    const scrollTop = messages.prop('scrollTop');
+    const scrollHeight = messages.prop('scrollHeight');
+    const newMessageHeight = newMessage.innerHeight();
+    const lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
         
 socket.on('connect', function () {
     console.log('Connected to server!');
@@ -15,6 +32,7 @@ socket.on('connect', function () {
         });
 
         jQuery('#messages').append(html);
+        scrollToBottom();
     });
 
     socket.on('newLocationMessage', function (message) {
@@ -27,6 +45,7 @@ socket.on('connect', function () {
         });
 
         jQuery('#messages').append(html);
+        scrollToBottom();
     });
 
     socket.on('welcomeGreeting', function (message) {
